646bb65d4ac94655a81681a87d4cf8374038105c
mcflugen <mcflugen@gmail.com>
2015-01-28 12:03:05 -0700

    Updated for new interface.

--- testing/set_value_test.cxx
+++ testing/set_value_test.cxx
@@ -1,64 +1,61 @@
-#include <bmi.hxx>
+#include <heat/bmi_heat.hxx>
 
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 
 void print_matrix (double *m, int n_dims, int * shape);
-void print_var_values (BMI::Model model, const char *var_name);
+void print_var_values (BmiHeat model, const char *var_name);
 
 int
 main (void)
 {
   int i;
   const int n_steps = 10;
-  BMI::Model model;
+  BmiHeat model;
   double *new_vals = NULL;
   int *shape = NULL;
-  int n_dims = 0;
+  int size;
+  char name[2048];
+  int rank;
 
-  model.initialize ("");
+  model.Initialize("");
 
-  std::cout << model.get_component_name () << std::endl;
+  model.GetComponentName(name);
+  std::cout << name << std::endl;
 
-  n_dims = model.get_var_rank ("surface_elevation");
-  shape = new int[n_dims];
-  model.get_grid_shape ("surface_elevation", shape);
+  model.GetVarRank("plate_surface__temperature", &rank);
 
-  new_vals = (double*)model.get_value_ptr ("surface_elevation");
+  shape = new int[rank];
+  model.GetGridShape("plate_surface__temperature", shape);
+
+  model.GetValuePtr("plate_surface__temperature", (char**)(&new_vals));
 
   fprintf (stdout, "Values before set\n");
   fprintf (stdout, "=================\n");
-  print_matrix (new_vals, n_dims, shape);
+  print_matrix (new_vals, rank, shape);
 
   new_vals[0] = -1;
 
-  { /* Allocate memory */
-    int len = 0;
+  model.GetVarSize("plate_surface__temperature", &size);
+  new_vals = new double[size];
 
-    len = shape[0] * shape[1];
-
-    new_vals = new double[len];
-    for (int i = 0; i < len; i++)
-      new_vals[i] = -1;
-  }
-
-  model.get_value ("surface_elevation", (void*)new_vals);
-  print_matrix (new_vals, n_dims, shape);
+  model.GetValue("plate_surface__temperature", (char*)new_vals);
+  print_matrix (new_vals, rank, shape);
 
   new_vals[0] = 1.;
 
-  model.set_double ("surface_elevation", new_vals);
+  model.SetValue("plate_surface__temperature", (char*)new_vals);
 
   fprintf (stdout, "Values after set\n");
   fprintf (stdout, "================\n");
-  print_matrix (new_vals, n_dims, shape);
+  print_matrix (new_vals, rank, shape);
 
 
   delete shape;
   delete new_vals;
 
-  model.finalize ();
+  model.Finalize ();
 
   return EXIT_SUCCESS;
 }
@@ -80,22 +77,25 @@ print_matrix (double *m, int n_dims, int * shape)
 }
 
 void
-print_var_values (BMI::Model model, const char *var_name)
+print_var_values(BmiHeat model, const char *var_name)
 {
-  int n_dims = model.get_var_rank (var_name);
-  int *shape = new int[n_dims];
+  int rank;
+  int *shape;
   double *var = NULL;
   int i, j;
 
-  model.get_grid_shape (var_name, shape);
+  model.GetVarRank(var_name, &rank);
+  shape = new int[rank];
+
+  model.GetGridShape(var_name, shape);
 
   var = new double[shape[0]*shape[1]];
 
-  model.get_double (var_name, var);
+  model.GetValue(var_name, (char*)var);
 
   fprintf (stdout, "Variable: %s\n", var_name);
 
-  print_matrix (var, n_dims, shape);
+  print_matrix (var, rank, shape);
 
   delete var;
   delete shape;
