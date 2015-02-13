af5eea56ec2a7e0538f896c0a867eae013d85b35
mcflugen <mcflugen@gmail.com>
2015-02-13 12:24:42 -0700

    Changed name of library to bmi_heatcxx.

--- heat/heatcxx.pc.cmake
+++ heat/heatcxx.pc.cmake
@@ -1,5 +1,5 @@
 Name: BMI
 Description: Basic Modeling Interface
 Version: ${BMI_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmicxx
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_heatcxx
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
