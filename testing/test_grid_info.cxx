fbd80c6e97c953ff2305a4c8ef8cc62449b3b3b4
Mark Piper <mark.piper@colorado.edu>
2019-11-12 10:56:38 -0700

    Add additional test information

--- testing/test_grid_info.cxx
+++ testing/test_grid_info.cxx
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 void print_var_info (BmiHeat model, char *var);
 
 int
@@ -58,11 +59,17 @@ print_var_info(BmiHeat model, char *var)
   char type[2048];
   char units[2048];
   int rank;
+  int size;
+  int nbytes;
+  char grid_type[2048];
 
   model.GetVarType(var, type);
   model.GetVarUnits(var, units);
+  nbytes = model.GetVarNbytes(var);
   grid = model.GetVarGrid(var);
   rank = model.GetGridRank(grid);
+  size = model.GetGridSize(grid);
+  model.GetGridType(grid, grid_type);
 
   shape = new int[rank];
   spacing = new double[rank];
@@ -79,13 +86,14 @@ print_var_info(BmiHeat model, char *var)
   fprintf (stdout, "Type: %s\n", type);
   fprintf (stdout, "Units: %s\n", units);
   fprintf (stdout, "Rank: %d\n", rank);
+  fprintf (stdout, "Size: %d\n", size);
+  fprintf (stdout, "Nbytes: %d\n", nbytes);
+  fprintf (stdout, "Grid: %s\n", grid_type);
   fprintf (stdout, "Dimension: %d x %d\n", shape[0], shape[1]);
   fprintf (stdout, "Resolution: %f x %f\n", spacing[0], spacing[1]);
-  fprintf (stdout, "Corner: %f x %f\n", origin[0], origin[1]);
+  fprintf (stdout, "Corner: (%f, %f)\n", origin[0], origin[1]);
 
   delete origin;
   delete spacing;
   delete shape;
-
-  return;
 }
