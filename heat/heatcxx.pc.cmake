bfad9d50e87e257b608b15cd36fa0476c4de61c9
Mark Piper <mark.piper@colorado.edu>
2023-10-23 14:24:29 -0600

    Update CMake build steps

--- heat/heatcxx.pc.cmake
+++ heat/heatcxx.pc.cmake
@@ -1,5 +1,5 @@
 Name: HeatCXX
 Description: 2D Heat Equation
 Version: ${HEAT_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmiheatcxx
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include
+Libs: -L${CMAKE_INSTALL_LIBDIR} -l${bmi_name}
+Cflags: -I${CMAKE_INSTALL_INCLUDEDIR}
