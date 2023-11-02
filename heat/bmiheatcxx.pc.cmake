bd4e3ed37d8381e62197b9b9f1ba493bf2afdab0
Mark Piper <mark.piper@colorado.edu>
2023-11-02 14:36:24 -0600

    Add pkg-config file for model BMI

---
+++ heat/bmiheatcxx.pc.cmake
@@ -0,0 +1,5 @@
+Name: BmiHeatCXX
+Description: BMI for HeatCXX model
+Version: ${CMAKE_PROJECT_VERSION}
+Cflags: -I${CMAKE_INSTALL_FULL_INCLUDEDIR}
+Libs: -L${CMAKE_INSTALL_FULL_LIBDIR} -l${bmi_name}
